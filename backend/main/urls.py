8c9b72a75142ae7db79c80f2279630b8ec29f4ee
sanghun <sangia070122@gmail.com>
2024-10-20 21:36:33 +0900

    initial

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CalendarValueViewSet
 
 router = DefaultRouter()
-router.register(r'calendar-values', CalendarValueViewSet, basename='calendar-values')
+router.register(r'calendar-values', CalendarValueViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
