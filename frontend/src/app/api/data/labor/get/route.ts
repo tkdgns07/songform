737fdf65c1e84950585d3bf5ea7ae073b0210067
tkdgns07 <tkdgns070122@gmail.com>
2024-11-04 11:23:19 +0900

    fix build error

--- frontend/src/app/api/data/labor/get/route.ts
+++ frontend/src/app/api/data/labor/get/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest) {
     try {
         const data = await prisma.laborCalendar.findMany();
-        NextResponse.json({ status : 200, data : data })
+        return NextResponse.json({ status: 200, data: data });
     } catch (error) {
-        NextResponse.json({ status : 500, error : 'Internal Server Error' })
+        console.error(error); // 에러 로그 추가
+        return NextResponse.json({ status: 500, error: 'Internal Server Error' });
     } finally {
         await prisma.$disconnect();
     }
