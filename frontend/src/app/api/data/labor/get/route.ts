c85b14db5a0cbe777d0f2f75c4c828aefaf3bc07
tkdgns07 <tkdgns070122@gmail.com>
2024-11-06 17:36:13 +0900

    dd

--- frontend/src/app/api/data/labor/get/route.ts
+++ frontend/src/app/api/data/labor/get/route.ts
@@ -1,17 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
-import prisma from "../../../../../../prisma/client"
+import prisma from "../../../../../../prisma/client";
 
 export async function GET(request: NextRequest) {
     try {
-        for (let i = 1; i < 5; i++){
+        for (let i = 1; i < 5; i++) {
             const data = await prisma.laborCalendar.findMany();
-            console.log(data)
-            if (data){
-                return NextResponse.json({ status: 200, message : 'Render success', data: data });
+            console.log(data);
+            
+            if (data && data.length > 0) {
+                return NextResponse.json({ status: 200, message: 'Render success', data: data });
             }
         }
-        return NextResponse.json({ status: 500, error: 'Internal Server Error' });
+        return NextResponse.json({ status: 404, error: 'No data found' });
     } catch (error) {
+        console.error(error);
         return NextResponse.json({ status: 500, error: 'Internal Server Error' });
     } finally {
         await prisma.$disconnect();
