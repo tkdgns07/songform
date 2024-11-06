b9e25028c75b2b5359f371c683bcda7e2cd49f76
tkdgns07 <tkdgns070122@gmail.com>
2024-11-06 17:22:48 +0900

    ddd

--- frontend/src/app/api/data/labor/get/route.ts
+++ frontend/src/app/api/data/labor/get/route.ts
@@ -5,6 +5,7 @@ export async function GET(request: NextRequest) {
     try {
         for (let i = 1; i < 5; i++){
             const data = await prisma.laborCalendar.findMany();
+            console.log(data)
             if (data){
                 return NextResponse.json({ status: 200, message : 'Render success', data: data });
             }
