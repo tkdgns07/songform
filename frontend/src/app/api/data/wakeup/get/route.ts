28c75d07ca77afbf8bd648c0b28425e43bec97eb
tkdgns07 <tkdgns070122@gmail.com>
2024-11-09 23:16:34 +0900

    dd

--- frontend/src/app/api/data/wakeup/get/route.ts
+++ frontend/src/app/api/data/wakeup/get/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from "../../../../../../prisma/client";
+import axios from 'axios';
 
 export async function GET(request: NextRequest) {
-
+    await axios.get(`${process.env.NEXTAUTH_URL}/api/data/test`)
     try {
         for (let i = 1; i < 5; i++) {
             const data = await prisma.wakeupCalendar.findMany();
@@ -10,7 +11,6 @@ export async function GET(request: NextRequest) {
             if (data && data.length > 0) {
                 return NextResponse.json({ status: 200, message: 'Render success', data: data });
             }
-
             await new Promise((resolve) => setTimeout(resolve, 1000));
         }
         return NextResponse.json({ status: 404, error: 'No data found' });
