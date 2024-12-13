c3671f1ee7e9de30c425468a78701b9bc8d5374f
tkdgns07 <tkdgns070122@gmail.com>
2024-12-13 17:39:39 +0900

    asdasd

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import SessionProvider from '@/components/SessionProvider';
-import LayoutContent from '@/components/layoutContent';
+import LayoutContent from '@/components/LayoutContent';
 
 const inter = Inter({ subsets: ['latin'] });
 
