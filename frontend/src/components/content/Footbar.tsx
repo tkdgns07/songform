7da658053666366169b885c5b17cd28138d34ea2
tkdgns07 <tkdgns070122@gmail.com>
2024-12-26 13:35:29 +0900

    last

--- frontend/src/components/content/Footbar.tsx
+++ frontend/src/components/content/Footbar.tsx
@@ -17,7 +17,8 @@ const Footbar = () => {
 					<div className="bg-lighttext w-[5px] h-[5px] rounded-full mr-[5px]"></div>
 					<p className="text-lighttext text-sm">Contact : sangia070122@gmail.com</p>
 					<div className="bg-lighttext w-[5px] h-[5px] rounded-full mr-[5px] ml-[20px]"></div>
-					<Link href={{ pathname : '/setadmin'  }} className="text-lighttext text-sm mr-[5px] hover:text-cusblue-normal duration-150">Set admin</Link>
+					{/* <Link href={{ pathname : '/setadmin'  }} className="text-lighttext text-sm mr-[5px] hover:text-cusblue-normal duration-150">Set admin</Link> */}
+					<p className="text-lighttext text-sm mr-[5px] hover:text-cusblue-normal duration-150 cursor-pointer">Set admin</p>
 					<div className="bg-lighttext w-[5px] h-[5px] rounded-full mr-[5px] ml-[20px]"></div>
 					<a href="https://northern-slice-716.notion.site/Songchan-13d5e69f5b6980f1a9c0fd243545d2e8?pvs=4">
 						<p className="text-lighttext text-sm hover:text-cusblue-normal duration-150">Guide Book</p>
